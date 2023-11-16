9ae79ebed635baaebf5d4eea1a36c584dd807eac
MatvejBeltsov <matvej.beltsov@gmail.com>
2023-11-16 11:03:11 +0300

    Решение задач

---
+++ Task_1/Task_1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Task_1
+{
+    
+    internal class Program
+    {
+        public static int Prompt(string message){
+            Console.WriteLine(message);
+            int result = Convert.ToInt32(Console.ReadLine());
+            return result;
+        
+        }
+
+
+        public static int[] InputArray(int length){
+            int[] array = new int[length];
+
+            for(int i = 0; i < array.Length; i++){
+                array[i] = Prompt($"Введите {i + 1} элемент");
+            }
+            return array;
+        }
+
+        public static void PrintArray(int[] array){
+            Console.Write(array[0]);
+            for (int i = 1; i < array.Length; i++){
+                Console.Write($", {array[i]}");
+            }
+            Console.WriteLine();
+        }
+
+        public static int CountPositiveElements(int[] array){
+            int count = 0;
+            for (int i = 0; i < array.Length; i++){
+                if(array[i] > 0){
+                    count++;
+                }
+            }
+            return count;
+        } 
+       
+        static void Main(string[] args)
+        {
+            int length = Prompt("Введите количество элементов > ");
+            int[] array;
+            array = InputArray(length);
+            PrintArray(array);
+            Console.WriteLine("Количество положительных элементов в массиве " + CountPositiveElements(array));
+            
+        }
+    }
+}
---
+++ Task_2/Task_2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace Task_2
+{
+    
+    internal class Program
+    {
+        const int COEFFICIENT = 0;
+        const int CONSTANTS = 1;
+        const int X_COORD = 0;
+        const int Y_COORD = 1;
+         const int Line1 = 1;
+        const int Line2 = 2;
+        
+        public static double[] InputLineData(int numberOfLine){
+            double[] linedata = new double[2];
+            linedata[COEFFICIENT] = Prompt($"Введите коэффициент для {numberOfLine} прямой > ");
+            linedata[CONSTANTS] = Prompt($"Введите константу для {numberOfLine} прямой > ");
+            return linedata;
+        }
+        
+        
+        public static int Prompt(string message){
+            Console.WriteLine(message);
+            int result = Convert.ToInt32(Console.ReadLine());
+            return result;
+        
+        }
+
+        public static double[] FindCoords(double[] lineData1, double[] lineData2){
+            double[] coord = new double[2];
+            coord[X_COORD] = (lineData1[CONSTANTS] - lineData2[CONSTANTS]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+            coord[Y_COORD] = lineData1[CONSTANTS] * coord[X_COORD] + lineData1[CONSTANTS];
+            return coord;
+        }
+ 
+        public static bool ValidateLine(double[] lineData1, double[] lineData2){
+            if(lineData1[COEFFICIENT] == lineData2[COEFFICIENT]){
+
+                if(lineData1[CONSTANTS] == lineData2[CONSTANTS]){
+                    Console.WriteLine("Прямые совпадают");
+                    return false;
+                }
+
+                else{
+                    Console.WriteLine("Прямые параллельны");
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        
+
+
+        
+       
+        static void Main(string[] args)
+        {
+            
+
+            double[] lineData1 = InputLineData(Line1);
+            double[] lineData2 = InputLineData(Line2);
+
+            if(ValidateLine(lineData1, lineData2)){
+                double[] coord = FindCoords(lineData1, lineData2);
+                Console.Write($"Точка пересечения уравнения y = {lineData1[COEFFICIENT]} * x + {lineData1[CONSTANTS]} и y = {lineData2[COEFFICIENT]} * x + {lineData2[CONSTANTS]}");
+                Console.WriteLine($" имеет координаты ({coord[X_COORD]}, {coord[Y_COORD]})");
+            }
+            
+        }
+    }
+}
